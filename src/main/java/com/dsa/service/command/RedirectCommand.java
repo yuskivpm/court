5ed7950c6ce13e4f5b4de0a3edd3dce9635aa830
Petro Yuskiv <yuskiv.pm@gmail.com>
2020-05-04 23:16:33 +0300

    redirect

---
+++ src/main/java/com/dsa/service/command/RedirectCommand.java
@@ -0,0 +1,26 @@
+package com.dsa.service.command;
+
+import com.dsa.controller.ControllerRequest;
+import com.dsa.controller.ControllerResponse;
+import com.dsa.controller.ResponseType;
+import com.dsa.service.resource.ConfigManager;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Function;
+
+public class RedirectCommand implements Function<ControllerRequest, ControllerResponse> {
+
+  @Override
+  public ControllerResponse apply(@NotNull ControllerRequest request) {
+    String pageName = request.getParameter("page");
+    String realPageName;
+    try {
+      realPageName = ConfigManager.getProperty("path.page." + pageName);
+    } catch (Exception e) {
+      realPageName = pageName;
+    }
+    return new ControllerResponse(ResponseType.FORWARD, realPageName);
+  }
+
+}
